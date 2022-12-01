9069cf1aadaeebef3d62e9dd27165a3d3b74e459
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-01 09:25:48 +0700

    adding navbar, header, brand, wgpt3 & features

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 function Brand() {
-  return (
-    <div>Brand</div>
-  )
+  return <div className="gpt3__brand section__padding">
+    <div className="">
+      <img src={google} alt="google" />
+    </div>
+    <div className="">
+      <img src={slack} alt="slack" />
+    </div>
+    <div className="">
+      <img src={atlassian} alt="atlassian" />
+    </div>
+    <div className="">
+      <img src={dropbox} alt="dropbox" />
+    </div>
+    <div className="">
+      <img src={shopify} alt="shopify" />
+    </div>
+  </div>;
 }
 
-export default Brand
+export default Brand;
